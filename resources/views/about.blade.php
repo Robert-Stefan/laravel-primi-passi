18aef2557acb4f032f879214ecc3a4db1be4928c
Robert-Stefan <robertboss05@hotmail.it>
2021-06-07 20:20:21 +0200

    Finish

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <header>
+            <h1>About</h1>
+        </header>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, quisquam inventore est temporibus similique incidunt ex maxime rerum alias sed tenetur nulla laborum expedita magni enim praesentium earum quam cupiditate.
+        </p>
+        <a href="{{ route('home') }}">HomePage</a>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// HOMEPAGE
+Route::get('/', function () {
+    return view('home');
+}) -> name('home');
+
+//ABOUT
+Route::get('/about', function () {
+    return view('about');
+}) -> name('about');
